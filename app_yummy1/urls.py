440c95935946a54a3bbe9f51dadff097635121c8
Reis567 <matheusdosreis9@gmail.com>
2025-05-09 20:34:21 -0300

    👌 IMPROVE: URL ITENS

--- app_yummy1/urls.py
+++ app_yummy1/urls.py
@@ -14,4 +14,14 @@ urlpatterns = [
     path('api/listas/<int:lista_id>/excluir/', excluir_lista, name='api_excluir_lista'),
     path('api/listas/<int:lista_id>/status/', atualizar_status_lista, name='api_atualizar_status_lista'),
     path('api/listas/estatisticas/', estatisticas_listas, name='api_estatisticas_listas'),
+
+
+    path('api/itens/criar/', criar_item, name='api_criar_item'),
+    path('api/itens/<int:item_id>/', obter_item, name='api_obter_item'),
+    path('api/itens/<int:item_id>/atualizar/', atualizar_item, name='api_atualizar_item'),
+    path('api/itens/<int:item_id>/excluir/', excluir_item, name='api_excluir_item'),
+    path('api/itens/<int:item_id>/status/', atualizar_status_item, name='api_atualizar_status_item'),
+    path('api/itens/<int:item_id>/prioridade/', atualizar_prioridade_item, name='api_atualizar_prioridade_item'),
+    path('api/listas/<int:lista_id>/itens/', listar_itens_da_lista, name='api_listar_itens_da_lista'),
+    
 ]
